1f40a65acd40408b1bb1871a6de30c487353aa00
Tomasz <dworowytomasz@gmail.com>
2016-11-19 18:22:53 +0100

    update

--- Staff/iteration.py
+++ Staff/iteration.py
@@ -36,3 +36,32 @@ print(max([1,2,3]))
 print(min([1,2,3]))
 print(any([1,2,3,"","Spam"]))
 print(all([1,2,3,"","Spam"]))
+
+
+M = map(abs,(-1,0,1))
+print(next(M)) #ubywa elementów , same for zip i filter()
+print(next(M))
+print(next(M))
+
+for x in M: print(x)
+
+R = range(3) #Range obsługuje wielokortne  niezależne iteracje
+I1 = iter(R)
+print(next(I1))
+print(next(I1))
+I2 = iter(R)
+print(next(I2))
+print(next(I2))
+
+
+
+D = dict(a=1,b=2,c=3)
+K = D.keys()
+I = iter(K)
+print(next(I))
+print(next(I))
+
+
+I = iter(D)
+print(next(I))
+print(next(I))
