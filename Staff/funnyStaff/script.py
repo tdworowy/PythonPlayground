41f8d8c03fc1ad61156c433e7b2561178f0da3c2
Tomasz <dworowytomasz@gmail.com>
2017-02-24 18:19:50 +0100

    update

--- Staff/funnyStaff/script.py
+++ Staff/funnyStaff/script.py
@@ -1,4 +1,16 @@
 import os
-print("HAHA")
+
 path = os.path.dirname(os.path.abspath(__file__))+ "\\script.py"
+path2 = os.path.dirname(os.path.abspath(__file__))+ "\\iteration.txt"
+f2 = open(path2,'r+')
+i = f2.read()
+i= int (i)
+print("HAHA",i)
+i+=1
+f2.seek(0)
+f2.truncate()
+
+f2.write(str(i))
+f2.flush()
+
 os.system("python < "+ path)
