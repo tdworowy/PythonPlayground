4829ed06e69cc519e648c99a50687428ba603d9d
Tomasz <dworowytomasz@gmail.com>
2017-04-08 17:37:19 +0200

    update

--- Staff/OOP/example/person.py
+++ Staff/OOP/example/person.py
@@ -15,14 +15,45 @@ class Person:
 
 
 class Manager(Person):
+
+    def __init__(self,name,pay=0):
+        Person.__init__(self,name,"Manager",pay)
+
     def giveRaise(self,precent,bonus=10):
-         Person.giveRaise(self,precent +bonus)
+         Person.giveRaise(self,precent + bonus)
+
+
+class Manager2:
+    def __init__(self,name,pay):
+        self.person = Person(name , 'Manager',pay)
+    def giveRaise(self,precent,bonus=10):
+        self.person.giveRaise(precent + bonus)
+
+    def __getattr__(self, attr):
+        return  getattr(self.person,attr)
+    def __str__(self):
+        return str(self.person)
+
+
+class Department:
+    def __init__(self,*args):
+        self.members = list(args)
+    def addMember(self,person):
+        self.members.append(person)
+
+    def giveRaises(self,percent):
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print(person)
 
 
 def test1():
     homer = Person("Homer Simposon")
     boJack = Person("BoJack Horseman ",job="Actor" ,pay=1000)
-    rick = Manager("Rick Sanches",'alcoholic',20000)
+    rick = Manager("Rick Sanches",20000)
     print(homer)
     print(boJack)
     print(rick)
@@ -31,6 +62,11 @@ def test1():
     rick.giveRaise(11)
     print(boJack)
     print(rick)
+
+    team = Department(boJack,rick)
+    team.addMember(homer)
+    team.giveRaises(10)
+    team.showAll()
 
 if __name__ == '__main__':
     test1()
