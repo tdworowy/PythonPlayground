7130a8b89979e4911d9ff8740959d23b3fb432b7
Tomasz <dworowytomasz@gmail.com>
2017-04-24 17:51:49 +0200

    update

---
+++ Staff/OOP/calss4.py
@@ -0,0 +1,14 @@
+class example1:
+    print("Test1")
+
+class example2:
+      def __init__(self):
+        print("Test2")
+
+class example3:
+       def pr(self):
+        print("Test3")
+
+a = example2()
+b = example3()
+b.pr()
---
+++ Staff/OOP/class5.py
@@ -0,0 +1,17 @@
+class ex1:
+    x = 1
+
+
+
+a =ex1()
+b =ex1()
+
+a.x = 10
+
+print(a.x)
+print(b.x)
+
+ex1.x = 11
+
+print(a.x)
+print(b.x)
