649500765e920e309f38d321e95106c2f9b05ede
Tomasz <dworowytomasz@gmail.com>
2017-04-29 22:44:50 +0200

    update

--- myUtils/randomString.py
+++ myUtils/randomString.py
@@ -5,3 +5,7 @@ from random import choice
 def randomString(size):
     val = "".join(choice(string.ascii_lowercase) for i in range(size))
     return val
+
+
+if __name__ == '__main__':
+    print(randomString(25))
