0d3dd96b891912f7df50dcb3d1870c307c31fae2
Tomasz <dworowytomasz@gmail.com>
2017-05-12 18:47:28 +0200

    update

--- UI/tkinterUI.py
+++ UI/tkinterUI.py
@@ -2,23 +2,19 @@ import tkinter
 from doctest import master
 from tkinter import messagebox
 
-
-
 class gui:
     def __init__(self):
 
         self.top = tkinter.Tk()
-        self.Cwidth = 600
-        self.Cheight = 300
+        self.width = 600
+        self.height = 300
 
         self.btn_text = tkinter.StringVar(master)
         self.btn_text.set("Display Rectangle")
-        self.canvas = tkinter.Canvas(master, width= self.Cwidth, height= self.Cheight)
+        self.canvas = tkinter.Canvas(master, width= self.width, height= self.height)
         self.button = tkinter.Button(master, textvariable=self.btn_text, command=self.callback)
         self.entryW = tkinter.Entry(master)
         self.entryH = tkinter.Entry(master)
-
-
 
         self.flag = False
 
@@ -29,9 +25,9 @@ class gui:
           y = 25
           dic ={'x':x , 'y':y, 'x1':int(self.entryW.get())+x, 'y1':int(self.entryH.get())+y }
           return dic
+
         except Exception as ex:
             messagebox.showinfo(ex)
-
 
 
 
@@ -60,9 +56,7 @@ class gui:
         self.entryW.pack()
         self.entryH.pack()
 
-
         self.top.mainloop()
-
 
 
 def main():
