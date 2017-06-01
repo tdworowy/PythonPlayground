050404d205d304b1a1a4809111b177c34690c07b
Tomasz <dworowytomasz@gmail.com>
2017-06-01 17:59:55 +0200

    update

--- algorithms/sorting/mergeSort.py
+++ algorithms/sorting/mergeSort.py
@@ -11,19 +11,19 @@ class mergesort:
         if len(list) < 2:
             return list
         mid = int(len(list)/2)
-        newList1 = self.sort(list[:mid])
-        newList2 = self.sort(list[mid:])
+        partList1 = self.sort(list[:mid])
+        partList2 = self.sort(list[mid:])
         i = 0
         j = 0
-        while i < len(newList1) and j < len(newList2):
-            if newList1[i] > newList2[j]:
-                result.append(newList2[j])
+        while i < len(partList1) and j < len(partList2):
+            if partList1[i] > partList2[j]:
+                result.append(partList2[j])
                 j+=1
             else:
-                result.append(newList1[i])
+                result.append(partList1[i])
                 i+=1
-        result.extend(newList1[i:])
-        result.extend(newList2[j:])
+        result.extend(partList1[i:])
+        result.extend(partList2[j:])
         return result
 
 
