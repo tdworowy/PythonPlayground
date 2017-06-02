125d582696a437e948186c5e0c72836de96a9d5e
Tomasz <dworowytomasz@gmail.com>
2017-06-02 17:34:44 +0200

    update

---
+++ algorithms/sorting/gnomeSort.py
@@ -0,0 +1,26 @@
+from random import randint
+
+
+class gnomesort:
+
+    def sort(self,list_):
+        pos = 0
+        list = list_[:]
+        while pos < len(list):
+            if pos ==0 or list[pos] >= list[pos -1]:
+                pos +=1
+            else:
+                list[pos],list[pos-1] = list[pos-1],list[pos]
+                pos -=1
+        return list
+
+
+if __name__ == '__main__':
+    list = [randint(0, 10000) for x in range(1000)]
+    gs = gnomesort()
+    sortedList = gs.sort(list)
+    print(sortedList)
+
+    list.sort()
+    print(list)
+    assert sortedList == list
