91594ba85c8c3a7ee701eacc5af1110e7b067d4f
Tomasz <dworowytomasz@gmail.com>
2017-06-08 22:11:40 +0200

    update

--- Staff/funnyStaff/ultraKill.py
+++ Staff/funnyStaff/ultraKill.py
@@ -4,11 +4,14 @@ import os
 #try kill all processes
 
 def ultraKill():
+
     os.system("tasklist /FO CSV > taskList.csv")
     with open("taskList.csv", 'r') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            os.system("TaskKill /PID " + str(row[1]))
+            x = "TaskKill /PID " + str(row[1])+" /F"
+            print(x)
+            os.system(x)
 
 
 if __name__ == "__main__":
