b1d9015a8dbcdc389970e9ae4d903e94497843e7
Tomasz <dworowytomasz@gmail.com>
2017-06-08 18:16:43 +0200

    update

---
+++ machineLearning/check.py
@@ -0,0 +1,20 @@
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import  lightning #sklearn
+print('lightning: {}'.format(lightning.__version__))
+
+# scikit-learn # don't work
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
