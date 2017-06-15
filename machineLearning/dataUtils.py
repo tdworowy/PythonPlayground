9a603ce4dfc79f145db5895b5e996eafd73ab342
Tomasz <dworowytomasz@gmail.com>
2017-06-15 15:31:19 +0200

    fix

--- machineLearning/dataUtils.py
+++ machineLearning/dataUtils.py
@@ -1,10 +1,20 @@
+from myUtils.randomString import randomString
+
+
 def getListPercent_head(list,percent):
     percent = int(len(list) * (percent/100))
-    retuntList = list[percent:]
+    retuntList = list[:percent]
     return retuntList
 
 
 def getListPercent_tail(list,percent):
     percent = int(len(list) * (percent/100))
-    retuntList = list[:percent]
+    retuntList = list[-percent:]
     return retuntList
+
+
+
+if __name__ == '__main__':
+    list1= list(randomString(150))
+    print(getListPercent_head(list1,10))
+    print(getListPercent_tail(list1, 10))
