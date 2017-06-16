09caae7e3f16c54ca1ddc79eb8157a56104c05a0
Tomasz <dworowytomasz@gmail.com>
2017-06-16 19:47:40 +0200

    refactor

--- algorithms/cryptography/cesarCrypt.py
+++ algorithms/cryptography/cesarCrypt.py
@@ -5,39 +5,29 @@ from myUtils.compareStrings import getDiff
 
 
 def CesarCrypt(text,index):
-    return ''.join([getLetterUP(char,index) for char in text.lower()])
+    return ''.join([getLetter(char,index,setIndexUP) for char in text.lower()])
 
 def CesarUnCrypt(text,index):
-    return ''.join([getLetterDOWN(char,index) for char in text.lower()])
+    return ''.join([getLetter(char,-index,setIndexDOWN) for char in text.lower()])
 
 
-def getLetterUP(char,jump):
-    if  char.isalpha():
+def getLetter(char,jump,setIndexFunction):
+    if char.isalpha():
         alpha = string.ascii_lowercase
         index = alpha.index(char)+jump
         while index not in range(0,len(alpha)):
-            index =setIndexUP(index,len(alpha))
-        return alpha[index]
-    else:
-        return char
-
-
-def getLetterDOWN(char,jump):
-    if  char.isalpha():
-        alpha = string.ascii_lowercase
-        index = alpha.index(char)-jump
-        while index not in range(0,len(alpha)):
-            index =setIndexDOWN(index,len(alpha))
+            index =setIndexFunction(index,len(alpha))
 
         return alpha[index]
     else:
         return char
+
 
 
 def setIndexUP(i,alphabetSize):
     index = i
     if index >= alphabetSize: index = alphabetSize - index
-    if index < 0: index =  index * -1
+    if index < 0: index = index * -1
     return index
 
 def setIndexDOWN(i,alphabetSize):
