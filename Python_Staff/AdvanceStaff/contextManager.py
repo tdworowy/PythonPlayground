5d66900575277ebafcfe76fa9fbcbc37d5332e26
Tomasz <dworowytomasz@gmail.com>
2017-06-25 15:04:22 +0200

    update

---
+++ Python_Staff/AdvanceStaff/contextManager.py
@@ -0,0 +1,23 @@
+class TraceBlock:
+    def message(self,arg):
+        print("Executing",arg)
+
+    def __enter__(self):
+        print("Start with block")
+        return  self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            print("Normal exit \n")
+        else:
+            print("Exception raised" , exc_type)
+            return False
+
+with TraceBlock() as action:
+    action.message("Test 1")
+    print("Done")
+
+with TraceBlock() as action:
+    action.message("Test 2")
+    raise TypeError
+    print("Not Done")
