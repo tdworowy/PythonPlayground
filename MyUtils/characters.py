c1b493bd0d1328048b0cd2b6c0022660bda4b0df
Tomasz <dworowytomasz@gmail.com>
2017-07-13 17:51:19 +0200

    Update

--- MyUtils/characters.py
+++ MyUtils/characters.py
@@ -1,5 +1,13 @@
 
 def characters(frm,to):
-    return ','.join([chr(x) for x in range(frm,to)])
+    # return ','.join([chr(x) for x in range(frm,to)]) #max 1114111
+    return [chr(x) for x in range(frm, to)]  # max 1114111
 
-
+if __name__ == '__main__':
+    with open("allChars.txt",'a',encoding='utf-32') as f:
+        list = characters(0,1114111)
+        for char in list:
+            try:
+                f.write(char)
+            except Exception:
+                continue
