324139bcd47d74cdce687dadde7b82b53675aa57
Tomasz <dworowytomasz@gmail.com>
2017-07-15 15:52:42 +0200

    Update

---
+++ Python_Staff/AdvanceStaff/bytes2.py
@@ -0,0 +1,13 @@
+
+
+list = [x for x in range(0,256)]
+print(bytes(list))
+chars = []
+for byte in bytes(list):
+    try:
+        chars.append(chr(byte))
+    except Exception:
+        continue
+
+print(','.join(chars))
+
---
+++ Python_Staff/AdvanceStaff/bytes3.py
@@ -0,0 +1,6 @@
+x = b'avedwereajkwbdkjwabmb 2m'
+print(list(x))
+y = [z+1 for z in list(x)]
+print(y)
+z = ([chr(z) for z in y ])
+print(z)
