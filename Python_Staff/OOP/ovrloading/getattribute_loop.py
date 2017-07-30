570a383a7e0a95f53b5c22e05458b04dd25a616e
Tomasz <dworowytomasz@gmail.com>
2017-07-30 13:35:52 +0200

    Update

---
+++ Python_Staff/OOP/ovrloading/getattribute_loop.py
@@ -0,0 +1,18 @@
+class getA:
+    def __getattribute__(self, item):
+        print("GET attribute")
+        x = self.other  # infinite loop_
+
+    def __getattribute__(self, item):
+        x = object.__getattribute__(self,"other") # now works ok
+
+
+    def __setattr__(self, key, value):
+        self.other = value # infinite loop
+
+    def __setattr__(self, key, value):
+        self.__dict__['other'] =value # now works ok
+
+x = getA()
+x.name = 'test'
+x.name
