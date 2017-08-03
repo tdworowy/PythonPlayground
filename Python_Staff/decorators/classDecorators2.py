75c9581a03dbbb5a2d3570f5a2dca90078f35459
Tomasz <dworowytomasz@gmail.com>
2017-08-03 19:22:53 +0200

    Update

---
+++ Python_Staff/decorators/classDecorators2.py
@@ -0,0 +1,20 @@
+def decorator(cls):
+    class wrapped:
+        def __init__(self,*args):
+            self.wrapped = cls(*args)
+        def __getattr__(self, item):
+            return getattr(self.wrapped,item)
+
+    return wrapped
+
+
+@decorator
+class c:
+    def __init__(self,x,y):
+        self.atr = 'spam'
+
+
+x = c(6,7)
+print(x)
+print(x.atr)
+print(dir(x))
