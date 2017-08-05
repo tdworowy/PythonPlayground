0288423f9548346729673310b5dd3f27f970968f
Tomasz <dworowytomasz@gmail.com>
2017-08-05 10:53:35 +0200

    Update

--- MyUtils/randomString.py
+++ MyUtils/randomString.py
@@ -1,11 +1,14 @@
 import string
-from random import choice
+from random import choices
 
 
 def generateRandomString(size):
-    return "".join(choice(string.ascii_lowercase) for i in range(size))
+    return "".join(choices(string.ascii_lowercase, k=size))
+
+def generateRandomStringAll(size):
+    return ''.join(choices(string.ascii_letters + string.digits, k=size))
 
 
 
 if __name__ == '__main__':
-    print(generateRandomString(100))
+    print(generateRandomStringAll(35))
