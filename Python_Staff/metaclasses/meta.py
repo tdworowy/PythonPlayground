af0220790de3e7320da65cfbe463444fad90fd5d
Tomasz <dworowytomasz@gmail.com>
2017-08-07 18:01:35 +0200

    Update

---
+++ Python_Staff/metaclasses/meta.py
@@ -0,0 +1,19 @@
+class Meta(type):
+    def __new__(metaclass, class_name,suppesrs,classdic):
+        print("In Meta :",class_name,suppesrs,classdic,sep='\n')
+        return type.__new__(metaclass, class_name,suppesrs,classdic)
+
+
+class Eggs:
+    pass
+
+
+class Spam(Eggs,metaclass=Meta):
+    data =1
+    def meth(self,arg):
+        print(arg)
+
+print('_'*15)
+print("Create instance")
+X = Spam()
+X.meth("Test")
