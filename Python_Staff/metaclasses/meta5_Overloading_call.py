4a8b6500183f861a56fc58713bb105c92724710c
Tomasz <dworowytomasz@gmail.com>
2017-08-08 17:22:43 +0200

    Update

---
+++ Python_Staff/metaclasses/meta5_Overloading_call.py
@@ -0,0 +1,31 @@
+class SuperMeta:
+    def __call__(self, classname, supers, classdic):
+        print("in SuperMeta.call: ", classname, supers, classdic, sep='\n')
+        Class = self.__new__(classname, supers, classdic)
+        self.__init__(Class,classname, supers, classdic)
+        return Class
+
+
+class SubMeta(SuperMeta):
+    def __new__(self,classname,supers,classdic):
+        print("in SubMeta.new: ",classname,supers,classdic,sep='\n')
+        return type(classname,supers,classdic)
+
+
+    def __init__(self,Class,classname,supers,classdic):
+        print("in SubMeta.init: ", classname, supers, classdic, sep='\n')
+        print("...... Object initialized. Class:",list(Class.__dict__.keys()))
+
+
+class Eggs:
+    pass
+
+print("Create class")
+class Spam(Eggs,metaclass=SubMeta()):# that example did not work
+    data = 1
+    def meth(self,arg):
+        pass
+
+print("Add instance")
+X =Spam()
+print("Data: ",X.data)
