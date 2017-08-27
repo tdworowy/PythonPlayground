f02e2d1edb228cfd90a5ec073f7c6211f11c82db
Tomasz <dworowytomasz@gmail.com>
2017-08-27 15:00:46 +0200

    Update

---
+++ Python_Staff/decorators/buffors_.py
@@ -0,0 +1,43 @@
+import hashlib
+import pickle
+import time
+
+cache = {}
+
+def is_obsolete(entry,duration):
+    return time.time() - entry['time'] > duration
+
+def compute_key(func,args,kw):
+    key = pickle.dumps((func.__name__,args,kw))
+    return hashlib.sha1(key).hexdigest()
+
+def memoize(duration =10):
+    def _memoize(func):
+        def __memoize(*args,**kw):
+            key = compute_key(func,args,kw)
+
+            if (key in cache and not is_obsolete(cache[key],duration)):
+                print("We have a winner !")
+                return cache[key]['value']
+
+            result = func(*args,**kw)
+            cache[key] = {'value': result,'time': time.time()}
+            return result
+        return __memoize
+    return _memoize
+
+
+if __name__ == '__main__':
+    @memoize()
+    def someFunc(a,b):
+        return a+b
+
+
+    print(someFunc(2,2))
+    print("Cache: %s" %cache)
+    print(someFunc(2,2))
+    print(someFunc(3,3))
+    print("Cache: %s" % cache)
+    print(someFunc(3,3))
+
+
