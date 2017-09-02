9a58700b13759dadcbf76089cde6fb02d2edbefd
Tomasz <dworowytomasz@gmail.com>
2017-09-02 10:52:09 +0200

    fix

--- Python_Staff/OOP/calss4.py
+++ Python_Staff/OOP/calss4.py
@@ -1,14 +1,14 @@
-class example1:
+class example1_:
     print("Test1")
 
-class example2:
+class example2_:
       def __init__(self):
         print("Test2")
 
-class example3:
+class example3_:
        def pr(self):
         print("Test3")
 
-a = example2()
-b = example3()
+a = example2_()
+b = example3_()
 b.pr()
