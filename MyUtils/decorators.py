a108d3e735f2dc32071c47212f0943518286df29
Tomasz <dworowytomasz@gmail.com>
2017-10-14 11:06:49 +0200

    fix

--- MyUtils/decorators.py
+++ MyUtils/decorators.py
@@ -1,9 +1,9 @@
-
-def cachExeption(f):
+def cach_exception(f):
     def func(*args):
         try:
             return f(*args)
         except Exception  as err:
             print(str(err))
             pass
+
     return func
