2f40a9ebd1b465692ca5cafae94e219ce3aa4e71
Tomasz <dworowytomasz@gmail.com>
2017-10-28 22:52:45 +0200

    refactor

--- openGL/example_.py
+++ openGL/example_.py
@@ -1,15 +1,16 @@
-import  OpenGL.GL as gl
-from OpenGL.GL import  shaders
+import OpenGL.GL as gl
+from OpenGL.GL import shaders
 
 
-class lazy_class_attribute:
-    def __init__(self,function):
+class Lazy_class_attribute:
+    def __init__(self, function):
         self.fget = function
 
     def __get__(self, obj, cls):
-            value = self.fget(obj or cls)
-            setattr(cls,self.fget.__name__,value)
-            return value
+        value = self.fget(obj or cls)
+        setattr(cls, self.fget.__name__, value)
+        return value
+
 
 class ObjectUsingShader:
     VERTEX_CODE = """
@@ -27,13 +28,15 @@ class ObjectUsingShader:
      }
     """
 
-    @lazy_class_attribute
+    @Lazy_class_attribute
     def shader_program(self):
         print("Compiling")
-        return shaders.compileProgram(shaders.compileShader(self.VERTEX_CODE,gl.GL_VERTEX_SHADER),shaders.compileShader(self.FRAGMENT_CODE,gl.GL_FRAGMENT_SHADER))
+        return shaders.compileProgram(shaders.compileShader(self.VERTEX_CODE, gl.GL_VERTEX_SHADER),
+                                      shaders.compileShader(self.FRAGMENT_CODE, gl.GL_FRAGMENT_SHADER))
+
 
 if __name__ == "__main__":
     x = ObjectUsingShader()
     x.shader_program()
-    #don't work :)
-    #TODO fix
+    # don't work :)
+    # TODO fix
