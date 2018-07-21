ccce6c5c19a391718463cad75c0d8b52d688ffa2
Tomasz <dworowytomasz@gmail.com>
2018-07-21 13:30:34 +0200

    update

--- Python_Staff/Tests/Unit_Tests.py
+++ Python_Staff/Tests/Unit_Tests.py
@@ -1,26 +1,27 @@
-#test
+# test
 import unittest
 
 
-def add(x,y):
-    return x+y
+def add(x, y):
+    return x + y
 
 
+class TestAdd(unittest.TestCase):
+    def test_add_positive_integers(self):
+        self.assertEquals(add(1, 2), 3)
 
-class Tests(unittest.TestCase):
-    def testAdd1(self):
-       assert add(1,2) == 3
+    def test_add_zero(self):
+        self.assertEquals(add(0, 0), 0)
 
-    def testAdd2(self):
-        assert add(0, 0) == 0
+    def test_add_one_negative(self):
+        self.assertEquals(add(10, -2), 8)
 
-    def testAdd2(self):
-        assert add(10, -2) == 8
+    def test_add_two_negative(self):
+        self.assertEquals(add(-2, -2), -4)
 
-    def testLetters(self):
-        assert add('x', 'z') == 'xz'
-
+    def test_add_chars(self):
+        self.assertEquals(add('x', 'z'), 'xz')
 
 
 if __name__ == '__main__':
-    test_program = unittest.main()
+    unittest.main()
--- Python_Staff/Tests/unitests1.py
+++ Python_Staff/Tests/unitests1.py
@@ -1,15 +1,20 @@
+print("I'm : ", __name__)
 
-print("I'm : ",__name__)
-def minmax(test,*args):
+
+def min_max(test, *args):
     res = args[0]
     for arg in args:
-        if test(arg,res):
+        if test(arg, res):
             res = arg
     return res
 
-def lessthan(x,y) : return  x < y
-def grtrthan (x,y): return  x > y
 
-if __name__ == '__main__': #tests , won't run if module is imported
-    print(minmax(lessthan,4,2,1,4,67,8,9,3,1))
-    print(minmax(grtrthan, 4, 2, 1, 4, 67, 8, 9, 3, 1))
+def less_than(x, y): return x < y
+
+
+def greater_than(x, y): return x > y
+
+
+if __name__ == '__main__':  # tests , won't run if module is imported
+    print(min_max(less_than, 4, 2, 1, 4, 67, 8, 9, 3, 1))
+    print(min_max(greater_than, 4, 2, 1, 4, 67, 8, 9, 3, 1))
