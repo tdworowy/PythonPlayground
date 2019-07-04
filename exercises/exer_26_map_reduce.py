3ffcdb77628eee11f5dba0d048003db752714895
Tomasz <dworowytomasz@gmail.com>
2019-07-04 07:04:21 +0200

    update

---
+++ exercises/exer_26_map_reduce.py
@@ -0,0 +1,39 @@
+from collections import OrderedDict
+
+
+class MapReduce:
+    def __init__(self):
+        self.intermediate = OrderedDict()
+        self.result = []
+
+    def emit_intermediate(self, key, value):
+        self.intermediate.setdefault(key, [])
+        self.intermediate[key].append(value)
+
+    def emit(self, value):
+        self.result.append(value)
+
+    def execute(self, data):
+        for record in data:
+            self.mapper(record)
+
+        for key in self.intermediate:
+            self.reducer(key, self.intermediate[key])
+
+        self.result.sort()
+        for item in self.result:
+            print ("{\"key\":\"" + item[0] + "\",\"value\":\"" + str(item[1]) + "\"}")
+
+    def mapper(self,record):
+        v1, v2 = record.split()
+        self.emit_intermediate(v1, v2)
+        mapReducer.emit_intermediate(v2, v1)
+
+    def reducer(self,key, list_of_values):
+        self.emit([key, len(list_of_values)])
+
+if __name__ == '__main__':
+    inputData = ["Joe Sue","Sue Phi", "Phi Joe", "Phi Alice"]
+    mapReducer = MapReduce()
+    mapReducer.execute(inputData)
+
