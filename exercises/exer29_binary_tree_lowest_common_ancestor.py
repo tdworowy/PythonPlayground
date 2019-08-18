c28650147f564816e20eaa52f114f02e21af9c35
Tomasz <dworowytomasz@gmail.com>
2019-08-18 11:23:41 +0200

    update

---
+++ exercises/exer29_binary_tree_lowest_common_ancestor.py
@@ -0,0 +1,52 @@
+from binarytree import  Node
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.value:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.value:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def lca(root, v1, v2):
+  node = root
+  while node:
+    if max(v1, v2) < node.value :
+      node = node.left
+    elif min(v1, v2) > node.value:
+      node = node.right
+    else:
+      break
+  return node
+
+tree = BinarySearchTree()
+arr = [9, 7, 8, 5, 6, 4, 3, 1]
+
+for i in arr:
+    tree.create(i)
+
+v = [1, 6]
+
+ans = lca(tree.root, v[0], v[1])
+print(ans.value)
+print(tree.root)
+
