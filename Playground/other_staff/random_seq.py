36e1aa8659dd893f3029c44ce6149d26ede9aa99
Tomasz <dworowytomasz@gmail.com>
2020-04-02 16:11:03 +0200

    update

---
+++ Playground/other_staff/random_seq.py
@@ -0,0 +1,21 @@
+from random import shuffle
+
+if __name__ == "__main__":
+    result = {}
+    for i in range(1, 10000):
+        A = list(range(1, 11))
+        shuffle(A)
+        shufled = A[:]
+        swaps = 0
+        for i in range(len(A)):
+
+            min_idx = i
+            for j in range(i + 1, len(A)):
+                if A[min_idx] > A[j]:
+                    min_idx = j
+            A[i], A[min_idx] = A[min_idx], A[i]
+            swaps += 1
+        result[swaps] = shufled
+
+    max_swaps = max(result.keys())
+    print(result[max_swaps])
