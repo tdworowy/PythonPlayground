137a36797855fed9961d2088db3be484a095e66e
Tomasz <dworowytomasz@gmail.com>
2020-05-12 14:36:13 +0200

    update

---
+++ Playground/other_staff/rectangle_with_squares.py
@@ -0,0 +1,20 @@
+def gcd(a, b):
+    while a > 0 and b > 0:
+        if a >= b:
+            a = a % b
+        else:
+            b = b % a
+
+    return max(a, b)
+
+
+def squares(n, m):
+    _gtc = gcd(n, m)
+    if _gtc == 1:
+        return n * m
+    else:
+        return n * m / _gtc ** 2
+
+
+assert squares(2, 2) == 1
+assert squares(6, 10) == 15
