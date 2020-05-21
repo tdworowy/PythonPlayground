94e4aa52759f7ed977c6677f9ebdf6bf00c02d15
Tomasz <dworowytomasz@gmail.com>
2020-05-21 13:55:07 +0200

    update

--- Playground/graph_theory/travelling_salesman_problem.py
+++ Playground/graph_theory/travelling_salesman_problem.py
@@ -17,6 +17,37 @@ def brute_force(g: nx.Graph) -> int:
     return min(cycles)
 
 
+def average(g: nx.Graph) -> float:
+    n = g.number_of_nodes()
+
+    sum_of_weights = sum(g[i][j]['weight'] for i in range(n) for j in range(i))
+    return 2 * sum_of_weights / (n - 1)
+
+
+def nearest_neighbors(g: nx.Graph) -> int:
+    current_node = 0
+    path = [current_node]
+    n = g.number_of_nodes()
+
+    for _ in range(n - 1):
+        next_node = None
+        min_edge = float("inf")
+
+        for v in g.nodes():
+            if g.has_edge(current_node, v) and v not in path:
+                if g[current_node][v]['weight'] < min_edge:
+                    min_edge = g[current_node][v]['weight']
+                    next_node = v
+
+        assert next_node is not None
+        path.append(next_node)
+        current_node = next_node
+
+    weight = sum(g[path[i]][path[i + 1]]['weight'] for i in range(g.number_of_nodes() - 1))
+    weight += g[path[-1]][path[0]]['weight']
+    return weight
+
+
 if __name__ == "__main__":
     g = nx.Graph()
     g.add_edge(0, 1, weight=2)
@@ -33,3 +64,5 @@ if __name__ == "__main__":
     assert (cycle_length(g, cycle2) == 6)
 
     print(brute_force(g))
+    print(average(g))
+    print(nearest_neighbors(g))
