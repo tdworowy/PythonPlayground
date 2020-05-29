4bedbb13049a18c175136e68a045b00196f8b3c6
Tomasz <dworowytomasz@gmail.com>
2020-05-29 16:08:50 +0200

    update

---
+++ Playground/Complexity/logistic_map_plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+def plot_logistic_map(r: float, x: float, iterations: int):
+    iterations_list = []
+    results_list = []
+    for i in range(iterations):
+        x = r * (x - x ** 2)
+        results_list.append(x)
+        iterations_list.append(i)
+
+    plt.plot(iterations_list, results_list)
+    plt.show()
+
+
+plot_logistic_map(2.0, 0.2, 20)  # fix point
+plot_logistic_map(3.0, 0.2, 20)  # cyclical
+plot_logistic_map(3.5, 0.2, 20)  # cyclical2
+plot_logistic_map(4.0, 0.2, 20)  # chaotic
+plot_logistic_map(4.0, 0.3, 20)  # chaotic2
