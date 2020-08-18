15d99b12591f01edbfddd56660dd331e3a803cdd
Tomasz <dworowytomasz@gmail.com>
2020-08-18 11:09:23 +0200

    update

--- Playground/Complexity/cellular_automata/_1D/cellular_automata_vizualization2.py
+++ Playground/Complexity/cellular_automata/_1D/cellular_automata_vizualization2.py
@@ -1,6 +1,6 @@
-import pygame
-import yaml
 
+import yaml
+import pygame
 from Playground.Complexity.cellular_automata._1D.cellular_automata import cellular_automata_step, generate_rule
 from Playground.Complexity.cellular_automata._1D.cellular_automata_vizualization1 import generate_random
 from Playground.Complexity.cellular_automata.utils.utils import RoundList
@@ -9,14 +9,9 @@ from Playground.Complexity.cellular_automata.utils.utils import RoundList
 class GUI:
     def __init__(self, width: int = 1920, height: int = 1080, cell_size: int = 5, color_count: int = 2,
                  neighborhood_size: int = 3, rule_number: int = 90):
-        pygame.init()
 
         self.width = width
         self.height = height
-        self.screen = pygame.display.set_mode((width, height))
-        pygame.display.set_caption('Cellular Automata')
-
-        pygame.display.flip()
 
         self.x = 0
         self.y = 0
@@ -57,7 +52,13 @@ class GUI:
         self.y += self.cell_size
         pygame.display.flip()
 
-    def run(self):
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption(f'rule: {self.rule_number} colors:{self.color_count} neighborhood_size:{self.neighborhood_size}')
+        pygame.display.flip()
+
+    def run(self, stop_when_full: bool = False):
         rule = generate_rule(self.rule_number,
                              self.neighborhood_size,
                              [i for i in range(self.color_count)])
@@ -70,6 +71,12 @@ class GUI:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            if stop_when_full and self.y == self.height:
+                self.running = False
+                pygame.quit()
+
+
+
 
 def read_config():
     with open("visualization_config.yaml") as file:
@@ -77,14 +84,18 @@ def read_config():
 
 
 if __name__ == "__main__":
-    rule_id = 4
-    config = read_config()
-    config = config[rule_id]
-    config = config.get(f"rule_{rule_id}")
-    print(config)
-    GUI(
-        color_count=config.get("colors"),
-        neighborhood_size=config.get("neighborhood_size"),
-        rule_number=config.get("rule")
+    rule_id = range(5)
+    visualizations = []
+    for id in rule_id:
+        config = read_config()
+        config = config[id]
+        config = config.get(f"rule_{id}")
+        visualizations.append(GUI(
+            color_count=config.get("colors"),
+            neighborhood_size=config.get("neighborhood_size"),
+            rule_number=config.get("rule")
 
-    ).run()
+        ))
+    for viz in visualizations:
+        viz.init()
+        viz.run(stop_when_full=True)
