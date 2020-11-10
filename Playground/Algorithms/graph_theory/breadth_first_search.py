1c3c44cbdb5e335ab169aba33d92adb1bc371659
Tomasz <dworowytomasz@gmail.com>
2020-11-10 11:11:40 +0100

    update

---
+++ Playground/Algorithms/graph_theory/breadth_first_search.py
@@ -0,0 +1,38 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def breadth_first_search(graph: dict, start: str) -> list:
+    visited = []
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return visited
+
+
+if __name__ == "__main__":
+    graph = {
+        'Amin': {'Wasim', 'Nick', 'Mike'},
+        'Wasim': {'Imran', 'Amin'},
+        'Imran': {'Wasim', 'Faras'},
+        'Faras': {'Imran'},
+        'Mike': {'Amin'},
+        'Nick': {'Amin'}
+    }
+
+    res1 = breadth_first_search(graph, 'Amin')
+    res2 = breadth_first_search(graph, 'Mike')
+
+    graph1 = nx.DiGraph(graph)
+
+    print(res1)
+    print(res2)
+
+    nx.draw(graph1, with_labels=True, node_color='y', node_size=800)
+    plt.show()
---
+++ Playground/Algorithms/graph_theory/depth_first_search.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def depth_first_search(graph: dict, start: str, visited: set = None) -> set:
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next_ in graph[start] - visited:
+        depth_first_search(graph, next_, visited)
+    return visited
+
+
+if __name__ == "__main__":
+    graph = {
+        'Amin': {'Wasim', 'Nick', 'Mike'},
+        'Wasim': {'Imran', 'Amin'},
+        'Imran': {'Wasim', 'Faras'},
+        'Faras': {'Imran'},
+        'Mike': {'Amin'},
+        'Nick': {'Amin'}
+    }
+
+    res1 = depth_first_search(graph, 'Amin')
+    print("_" * 10)
+    res2 = depth_first_search(graph, 'Mike')
+
+    graph1 = nx.DiGraph(graph)
+
+    print(res1)
+    print(res2)
+
+    nx.draw(graph1, with_labels=True, node_color='y', node_size=800)
+    plt.show()
