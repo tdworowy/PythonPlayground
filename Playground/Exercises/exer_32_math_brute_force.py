180a99c83847d69a3a56493b56a339a49a0fb328
Tomasz <dworowytomasz@gmail.com>
2020-12-21 13:35:17 +0100

    update

---
+++ Playground/Exercises/exer_32_math_brute_force.py
@@ -0,0 +1,11 @@
+
+# take to mach time
+if __name__ == '__main__':
+    for i in range(9999999999999, 1, -1):
+        number = [char for char in str(i)]
+        number[0], number[-1] = number[-1], number[0]
+        number = int("".join(number))
+        if number == i * 2:
+            print(i)
+            break
+
