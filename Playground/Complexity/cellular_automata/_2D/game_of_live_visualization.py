92f923cd6836f4abedf4fd478c82385ccc462796
Tomasz <dworowytomasz@gmail.com>
2021-01-01 16:42:27 +0100

    move cellular automata to new repo

--- Playground/Complexity/cellular_automata/_2D/game_of_live_visualization.py
+++
@@ -1,7 +0,0 @@
-from Playground.Complexity.cellular_automata._2D.general_2d_automata import \
-    game_of_live_rules
-
-from Playground.Complexity.cellular_automata._2D.general_visualization import CellularAutomata2DVisualization
-
-if __name__ == "__main__":
-    CellularAutomata2DVisualization(game_of_live_rules, width=1085, height=1085, cell_size=10).main_loop()
