b672822ce9ba18f092e6ed23849977d21d9cb588
Tomasz <dworowytomasz@gmail.com>
2021-03-07 16:05:12 +0100

    update

---
+++ Playground/Exercises/exer35_number_format.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    w = len(f'{number:b}')
+    for n in range(1, number + 1):
+        print(f'{n:={w}} {n:={w}o} {n:={w}X} {n:={w}b}')
+
+
+if __name__ == '__main__':
+    print_formatted(99)
