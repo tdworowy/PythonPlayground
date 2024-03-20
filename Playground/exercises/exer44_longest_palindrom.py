fcc9396a3757850dfc2d2951d3ade0007d9241a8
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-03-20 10:26:30 +0100

    update

---
+++ Playground/exercises/exer44_longest_palindrom.py
@@ -0,0 +1,7 @@
+#TODO
+def longest_palindrome(s: str) -> str:
+  pass
+
+if __name__ == "__main__":
+     assert longest_palindrome("babad") == "bab"
+     assert longest_palindrome("cbbd") == "cbbd"
