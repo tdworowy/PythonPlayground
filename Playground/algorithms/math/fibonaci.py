aef3ad49e000422bb5d3f92b844a555305734b9a
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-10-12 09:57:03 +0200

    update

--- Playground/algorithms/math/fibonaci.py
+++ Playground/algorithms/math/fibonaci.py
@@ -5,8 +5,35 @@ from Playground.my_utils.staff.timer3 import timer3
 sys.setrecursionlimit(10000)
 
 
+def memoize(f):
+    call_cache = {}
+
+    def _memoized(arg):
+        try:
+            return call_cache[arg]
+        except KeyError:
+            return call_cache.setdefault(arg, f(arg))
+
+    return _memoized
+
+
 @timer3
-def fib1(count):
+def fib1a(count):
+    def _fib():
+        a, b = 0, 1
+        while 1:
+            yield b
+            a, b = b, a + b
+
+    f = _fib()
+    for i in range(count + 1):
+        x = next(f)
+    return x
+
+
+@memoize
+@timer3
+def fib1b(count):
     def _fib():
         a, b = 0, 1
         while 1:
@@ -20,7 +47,17 @@ def fib1(count):
 
 
 @timer3
-def fib2(count):
+def fib2a(count):
+    a, b = 0, 1
+    for i in range(count):
+        a, b = b, a + b
+
+    return b
+
+
+@memoize
+@timer3
+def fib2b(count):
     a, b = 0, 1
     for i in range(count):
         a, b = b, a + b
@@ -29,44 +66,23 @@ def fib2(count):
 
 
 @timer3
-def fib3(count):
+def fib3a(count):
     if count < 2:
         return 1
     else:
-        return fib3(count - 1) + fib3(count - 2)
-
-
-def memoize(f):
-    call_cache = {}
-
-    def _memoized(arg):
-        try:
-            return call_cache[arg]
-        except KeyError:
-            return call_cache.setdefault(arg, f(arg))
-
-    return _memoized
+        return fib3a(count - 1) + fib3a(count - 2)
 
 
 @memoize
 @timer3
-def fib4(count):
+def fib3b(count):
     if count < 2:
         return 1
     else:
-        return fib4(count - 1) + fib4(count - 2)
-
-
-@memoize
-@timer3
-def fib5(count):
-    if count < 2:
-        return 1
-    else:
-        return fib5(count - 1) + fib5(count - 2)
+        return fib3b(count - 1) + fib3b(count - 2)
 
 
 if __name__ == "__main__":
-    functions = [fib1, fib2, fib3, fib4, fib4, fib5, fib5]
+    functions = [fib1a, fib1b, fib2a, fib2b, fib3a, fib3b]
     for i, f in enumerate(functions):
-        print(f"{i} {f(35)}")
+        print(f"{i} {f(90)}")
